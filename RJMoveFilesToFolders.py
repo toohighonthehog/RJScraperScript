aa9c3a1225f16a1fdb7cc0b1c80329130abbf924
Richard Johnson <richardjj627@gmail.com>
2023-09-05 07:53:19 +0100

    crawl depth

--- RJMoveFilesToFolders.py
+++ RJMoveFilesToFolders.py
@@ -7,22 +7,29 @@ start_directory = "/path/to/your/directory"
 # Define the file extension you want to search for
 target_extension = ".txt"
 
-# Iterate through the directory and its subdirectories
+# Maximum depth to search (3 levels deep)
+max_depth = 3
+
+# Function to move a file to a folder with the same name as its extension
+def move_file_to_extension_folder(file_path):
+    _, file_name = os.path.split(file_path)
+    extension = os.path.splitext(file_name)[1]
+    extension_folder = os.path.join(os.path.dirname(file_path), extension.lstrip('.'))
+    
+    if not os.path.exists(extension_folder):
+        os.makedirs(extension_folder)
+    
+    destination_path = os.path.join(extension_folder, file_name)
+    shutil.move(file_path, destination_path)
+    print(f"Moved '{file_name}' to '{extension_folder}'")
+
+# Recursively search and move files
 for root, _, files in os.walk(start_directory):
+    depth = root.count(os.path.sep) - start_directory.count(os.path.sep)
+    if depth > max_depth:
+        continue
+    
     for file in files:
         if file.endswith(target_extension):
-            # Get the full path of the file
             file_path = os.path.join(root, file)
-
-            # Extract the extension without the dot
-            extension_without_dot = target_extension.strip(".")
-
-            # Create a folder with the same name as the extension if it doesn't exist
-            folder_path = os.path.join(root, extension_without_dot)
-            if not os.path.exists(folder_path):
-                os.makedirs(folder_path)
-
-            # Move the file to the folder
-            destination_path = os.path.join(folder_path, file)
-            shutil.move(file_path, destination_path)
-            print(f"Moved '{file}' to '{folder_path}'")
+            move_file_to_extension_folder(file_path)
