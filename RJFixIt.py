a29608a87f7024ad81d6078b7349e9c3757c76ae
Richard Johnson <richardjj627@gmail.com>
2023-09-07 15:24:10 +0100

    Fully functional

--- RJFixIt.py
+++ RJFixIt.py
@@ -1,90 +1,135 @@
-import os, re, requests
+import os, re, requests, json
 from requests_html import HTMLSession
+from javscraper import *
+javlibrary = JAVLibrary()
 
-def move_to_folder(process_folder, process_extension):
-    result = ""
-    for file in os.listdir(process_folder):
-        if os.path.isfile(process_folder + file) and file.lower().endswith(process_extension):
-            print(f"Moving : {file}")
-            # Get the full path of the file and extract the extension without the dot
-            file_without_extension = re.sub(process_extension, '', file, flags=re.IGNORECASE)
-            extension_without_dot = process_extension.strip(".")
+# add a rerun option + re-get json
+# and some logic to check we're not nesting deeper and deeper
 
-            # Create a folder with the same name as the extension if it doesn't exist
-            destination_directory = process_folder + insert_hyphen_between_letters_and_numbers(file_without_extension.upper())
 
-            print(destination_directory)
+def move_to_directory(process_directory, process_file, process_extension):
+    process_result = ""
 
-            if not os.path.exists(destination_directory):
-                os.makedirs(destination_directory)
 
-            # Move the file to the folder
-            print(process_folder + file)
-            print(insert_hyphen_between_letters_and_numbers(destination_directory) + "/" + insert_hyphen_between_letters_and_numbers(file_without_extension).upper() + process_extension.lower())
-            os.rename(process_folder + file, insert_hyphen_between_letters_and_numbers(destination_directory) + "/" + insert_hyphen_between_letters_and_numbers(file_without_extension).upper() + process_extension.lower())
-            print(f"Moved '{file}' to '{destination_directory}'")
-            result = file_without_extension
-            print("======================================")
+
+    # Get the full path of the file and extract the extension without the dot
+    file_without_extension = re.sub(process_extension, '', process_file, flags=re.IGNORECASE)
+    extension_without_dot = process_extension.strip(".")
+
+    # Create a folder with the same name as the extension if it doesn't exist
+    destination_directory = process_directory + insert_hyphen_between_letters_and_numbers(file_without_extension.upper())
+    if not os.path.exists(destination_directory):
+        os.makedirs(destination_directory)
+
+    # Move the file to the folder
+    #print(process_directory + file)
+    #print(insert_hyphen_between_letters_and_numbers(destination_directory) + "/" + insert_hyphen_between_letters_and_numbers(file_without_extension).upper() + process_extension.lower())
+    os.rename(process_directory + file, insert_hyphen_between_letters_and_numbers(destination_directory) + "/" + insert_hyphen_between_letters_and_numbers(file_without_extension).upper() + process_extension.lower())
+    print(f"Moved :                 {process_file} to {destination_directory}")
+    process_result = file_without_extension
     
-    return result
+    return process_result
 
 def insert_hyphen_between_letters_and_numbers(input_string):
     # Use regular expression to find all occurrences of letters followed by numbers
     # and insert a hyphen between them
+    # it should only be a single hyphen so needs some checking
     result = re.sub(r'([a-zA-Z])(\d)', r'\1-\2', input_string)
     return result
 
-def download_subtitlecat(process_folder, process_title, process_language):
-    process_folder = process_folder + process_title + "/"
-    subfolder_title = process_title
-    process_title = insert_hyphen_between_letters_and_numbers(title)
+def download_subtitlecat(process_directory, process_title, process_language):
+    process_directory = process_directory + process_title + "/"
+    process_title = insert_hyphen_between_letters_and_numbers(process_title)
 
-    print(process_folder)
-    print(process_title)
+    print(f"Process Directory :     {process_directory}")
+    print(f"Process Title :         {process_title}")
 
     # Create the HTML session
     session = HTMLSession()
+    url_level1 = 'https://www.subtitlecat.com/index.php?search=' + process_title
+    response_level1 = session.get(url_level1)
 
-    # Define the URL
-    base_url = 'https://www.subtitlecat.com/index.php?search=' + process_title
-    response_level1 = session.get(base_url)
+    table_level1 = response_level1.html.find('table')[0]
+    table_level1_entries = [[c.absolute_links for c in row.find('td')][:1] for row in table_level1.find('tr')][1:]
 
-    table = response_level1.html.find('table')[0]
-    table_data = [[c.absolute_links for c in row.find('td')][:1] for row in table.find('tr')][1:]
-
-    for table_entry in table_data:
-        fullurl = (list(table_entry[0])[0])
-        if re.search(process_title, fullurl, re.IGNORECASE):
-            response_level2 = session.get(fullurl)
-            subslist = response_level2.html.xpath('/html/body/div[4]/div/div[2]', first=True)
-            for suburl in subslist.absolute_links:
-                if re.search(process_language, suburl, re.IGNORECASE):
-                    suburlresult = suburl
+    for table_level1_entry in table_level1_entries:
+        table_level1_entry_url = (list(table_level1_entry[0])[0])
+        if re.search(process_title, table_level1_entry_url, re.IGNORECASE):
+            response_level2 = session.get(table_level1_entry_url)
+            table_level2 = response_level2.html.xpath('/html/body/div[4]/div/div[2]', first=True)
+            for table_level2_entry in table_level2.absolute_links:
+                if re.search(process_language, table_level2_entry, re.IGNORECASE):
+                    subtitle_url = table_level2_entry
             try:
-                if re.search(process_language, suburlresult, re.IGNORECASE):
-                    suburlresultcheck = (requests.head(suburlresult).status_code)
-                    if suburlresultcheck==200:
-                        print(suburlresult)
+                if re.search(process_language, subtitle_url, re.IGNORECASE):
+                    subtitle_url_check = (requests.head(subtitle_url).status_code)
+                    if subtitle_url_check==200:
+                        print(f"Subtitle_URL :          {subtitle_url}")
                         # Split out the filename
-                        if url.find('/'):
-                            filename = ((suburlresult.rsplit('/', 1)[1]).lower())
-                        print("Download " + filename)
-                        rfile = requests.get(suburlresult, allow_redirects=True)
-                        print(process_folder  + filename)
-                        open(process_folder + filename, 'wb').write(rfile.content)
-                        new_filename = filename.replace(process_title, (process_title).upper())
-                        os.rename(filename, new_filename)
-                        print("======================================")
+                        if subtitle_url.find('/'):
+                            subtitle_filename = ((subtitle_url.rsplit('/', 1)[1]).lower())
+                        print(f"Downloading :           '{subtitle_filename}'")
+                        subtitle_download = requests.get(subtitle_url, allow_redirects=True)
+                        open(process_directory + subtitle_filename, 'wb').write(subtitle_download.content)
+                        new_subtitle_filename = re.sub(process_title, process_title.upper(), subtitle_filename, flags=re.IGNORECASE)
+                        os.rename(process_directory + subtitle_filename, process_directory + new_subtitle_filename)
             except:
                 pass
 
+
+
+
+def download_metadata(process_directory, process_title):
+    process_title = insert_hyphen_between_letters_and_numbers(process_title)
+    metadata = javlibrary.get_video(process_title)
+    if metadata is not None:
+        metadata_array = { "Code": metadata.code, "Name": metadata.name, "Actresses": metadata.actresses, "Studio": metadata.studio, "Image": metadata.image, "Genres": metadata.genres, "Score": metadata.score}
+        metadata_json = json.dumps(metadata_array, indent=4)
+        print(f"Downloading Metadata :  {process_title}.json")    
+        with open(process_directory + process_title + "/" + process_title + ".json", "w") as outfile:
+            outfile.write(metadata_json)
+    else:
+        print(f"No Metadata for :       {process_title}")
+
+def move_down_level(process_directory, process_file, process_extension):
+    file_without_extension = re.sub(process_extension, '', process_file, flags=re.IGNORECASE)
+    process_title = insert_hyphen_between_letters_and_numbers(file_without_extension)
+    source_file = process_directory + process_title + "/" + process_title + process_extension
+    destination_file = process_directory + process_title + process_extension
+    if os.path.exists(source_file):
+        #print(source_file)
+        #print(destination_file)
+        print(f"Moving Downlevel :       {source_file}")
+        try:
+            os.rename(source_file, destination_file)
+        except:
+            pass
+
 if __name__ == "__main__":
     # Define the directory you want to start the search + the file extension + language suffix
-    base_directory= "/mnt/multimedia/Other/RatedFinalJ/<path>/12/"
+    base_directory= "/mnt/multimedia/Other/RatedFinalJ/censored/12/"
     target_extension = ".mp4"
-    language="en.srt"
+    target_language = "en.srt"
+
+    print("============================================================================")
+
+    start_dir = os.listdir(base_directory)
+
+    # Moves the files down level so they get rescanned.
+    for file in start_dir:
+        if os.path.isdir(base_directory + file):
+            move_down_level(base_directory, file, target_extension)
+
+    # Do the scanning.
+    for file in os.listdir(base_directory):
+        if os.path.isfile(base_directory + file) and file.lower().endswith(target_extension):
+            to_be_scraped = move_to_directory(base_directory, file, target_extension)
+            print("----------------------------------------------------------------------------")
+            download_subtitlecat(base_directory, to_be_scraped, target_language)
+            print("----------------------------------------------------------------------------")
+            download_metadata(base_directory, to_be_scraped)
+            print("============================================================================")
+
+
+
     
-    title = move_to_folder(base_directory, target_extension)
-    
-    if len(title) > 0:
-        download_subtitlecat(start_directory, title, language)
