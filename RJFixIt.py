1d5b2c1d8baf8e332ca62bfb2815193bb7a228f2
Richard Johnson <richardjj627@gmail.com>
2023-09-18 18:51:52 +0100

    added regions

--- RJFixIt.py
+++ RJFixIt.py
@@ -37,6 +37,23 @@ REDO_FILES = True
 #t Add link to results.  As a table (as they can give multiple results?) or just the first result?
 #  Semi-modularize (so that the functions run when imported) - started
 
+
+#region Main Functions
+def move_down_level(process_file):
+    #file_without_extension = re.sub(process_extension, '', process_file, flags=re.IGNORECASE)
+    
+    folder_list_1 = os.listdir(BASE_DIRECTORY + process_file)
+    folder_list_2 = [file for file in folder_list_1 if any(file.endswith(ext) for ext in TARGET_EXTENSIONS)]
+
+    for filename in folder_list_2:
+    
+        #process_title = fix_file_code(file_without_extension)
+        source_file = BASE_DIRECTORY + process_file + "/" + filename
+        destination_file = TARGET_DIRECTORY + filename
+
+        my_logger.info("MOV - Moving " + source_file + " up a level.")
+        os.rename(source_file, destination_file)
+
 def move_to_directory(process_file, process_extension):
     process_result = False
     source_file_without_extension = re.sub(process_extension, '', process_file, flags=re.IGNORECASE)
@@ -66,140 +83,6 @@ def move_to_directory(process_file, process_extension):
     process_result = destination_file_without_extension
 
     return process_result
-
-def fix_file_code(input_string, delimiter = "-"):
-    letters = ""
-    numbers = ""
-    suffix = ""
-    filename, file_extension = os.path.splitext(input_string)
-    filename_original = filename
-    filename = filename.upper()
-    file_extension = file_extension.lower()
-
-    counter = 0
-    filename_length = len(filename)
-    
-    # get letters
-    while counter < filename_length:
-        char = filename[counter]
-
-        if ord(char) in range(64, 91):
-            letters = letters + char
-            counter = counter + 1
-        if char == delimiter:
-                counter = counter + 1
-                break
-        if (ord(char) in range(47, 58)):
-                break
-
-    while counter < filename_length:
-        char = filename[counter]
-
-        if ord(char) in range(47, 58):
-            numbers = numbers + char
-            counter = counter + 1
-        else:
-            break
-    
-    while counter < filename_length:
-        char = filename_original[counter]
-        counter = counter + 1
-        suffix = suffix + char
-
-    number = int(numbers)
-
-    return f"{letters}{delimiter}{number:03}{suffix}{file_extension}"
-
-def search_for_title(input_string, delimiter = "-"):
-    filename, file_extension = os.path.splitext(input_string)
-    filename = filename.upper()
-    file_extension = file_extension.lower()
-    pattern8 = r'^[A-Za-z]{5}\d{3}$'
-    pattern7 = r'^[A-Za-z]{4}\d{3}$'
-    pattern6 = r'^[A-Za-z]{3}\d{3}$'
-    pattern5 = r'^[A-Za-z]{2}\d{3}$'
-
-    results = []
-    filename = re.sub(delimiter, '', filename, flags=re.IGNORECASE)
-    filename_length = len(filename)
-
-    # Search for 8 character codes.
-    counter = 0
-    while counter + 7 < filename_length:
-        input_string = filename[counter:counter + 8]
-        if (re.match(pattern8, input_string)):
-            if my_javlibrary_new_search(input_string):
-                results.append(filename[counter:counter + 8])
-        counter = counter + 1    
-    
-    # Search for 7 character codes.
-    counter = 0
-    while counter + 6 < filename_length:
-        input_string = filename[counter:counter + 7]
-        if (re.match(pattern7, input_string)):
-            if my_javlibrary_new_search(input_string):
-                results.append(filename[counter:counter + 7])
-        counter = counter + 1
-
-    # Search for 6 character codes.
-    counter = 0
-    while counter + 5 < filename_length:
-        input_string = filename[counter:counter + 6]
-        if (re.match(pattern6, input_string)):
-            if my_javlibrary_new_search(input_string):
-                results.append(filename[counter:counter + 6])
-        counter = counter + 1
-
-    # Search for 5 character codes.
-    counter = 0
-    while counter + 4 < filename_length:
-        input_string = filename[counter:counter + 5]
-        if (re.match(pattern5, input_string)):
-            if my_javlibrary_new_search(input_string):
-                results.append(filename[counter:counter + 5])
-        counter = counter + 1
-
-    results = remove_substrings(results)
-
-    return results
-
-def my_javlibrary_new_search(function_input_string):
-    my_javlibrary = JAVLibrary()
-    function_count = 0
-    result = []
-    while len(result) == 0 and function_count <= 6:
-        result = my_javlibrary.search(function_input_string)
-        time.sleep(0.25)
-        function_count = function_count + 1
-    return result
-
-def my_javlibrary_new_getvideo(function_input_string):
-    my_javlibrary = JAVLibrary()
-    function_count = 0
-    result = ""
-    while result == "" and function_count <= 6:
-        result = my_javlibrary.get_video(function_input_string)
-        time.sleep(0.25)
-        function_count = function_count + 1
-    return result
-
-def remove_substrings(strings):
-    # Sort the strings by length in descending order
-    strings.sort(key=len, reverse=True)
-    
-    # Initialize a list to store non-substring strings
-    result = []
-    
-    # Iterate through the sorted strings
-    for s in strings:
-        # Check if the current string is a substring of any previously added string
-        is_substring = any(s in r for r in result)
-        
-        # If it's not a substring of any previous string, add it to the result
-        if not is_substring:
-            result.append(s)
-    
-    return result
 
 def download_subtitlecat(process_title):
     process_directory = TARGET_DIRECTORY + process_title + "/"
@@ -320,32 +203,134 @@ def send_data_to_database(process_metadata, process_metadata_urls, process_locat
         my_cursor.execute(my_insert_sql_title_urls, (process_metadata.code, u, hash_output))
 
     my_connection.commit()
+#endregion
 
-def move_down_level(process_file):
-    #file_without_extension = re.sub(process_extension, '', process_file, flags=re.IGNORECASE)
+#region Wrapped Scraper Functions
+def my_javlibrary_new_search(function_input_string):
+    my_javlibrary = JAVLibrary()
+    function_count = 0
+    result = []
+    while len(result) == 0 and function_count <= 6:
+        result = my_javlibrary.search(function_input_string)
+        time.sleep(0.25)
+        function_count = function_count + 1
+    return result
+
+def my_javlibrary_new_getvideo(function_input_string):
+    my_javlibrary = JAVLibrary()
+    function_count = 0
+    result = ""
+    while result == "" and function_count <= 6:
+        result = my_javlibrary.get_video(function_input_string)
+        time.sleep(0.25)
+        function_count = function_count + 1
+    return result
+#endregion
+
+#region Primary Data Functions
+def fix_file_code(input_string, delimiter = "-"):
+    letters = ""
+    numbers = ""
+    suffix = ""
+    filename, file_extension = os.path.splitext(input_string)
+    filename_original = filename
+    filename = filename.upper()
+    file_extension = file_extension.lower()
+
+    counter = 0
+    filename_length = len(filename)
     
-    folder_list_1 = os.listdir(BASE_DIRECTORY + process_file)
-    folder_list_2 = [file for file in folder_list_1 if any(file.endswith(ext) for ext in TARGET_EXTENSIONS)]
+    # get letters
+    while counter < filename_length:
+        char = filename[counter]
 
-    for filename in folder_list_2:
+        if ord(char) in range(64, 91):
+            letters = letters + char
+            counter = counter + 1
+        if char == delimiter:
+                counter = counter + 1
+                break
+        if (ord(char) in range(47, 58)):
+                break
+
+    while counter < filename_length:
+        char = filename[counter]
+
+        if ord(char) in range(47, 58):
+            numbers = numbers + char
+            counter = counter + 1
+        else:
+            break
     
-        #process_title = fix_file_code(file_without_extension)
-        source_file = BASE_DIRECTORY + process_file + "/" + filename
-        destination_file = TARGET_DIRECTORY + filename
+    while counter < filename_length:
+        char = filename_original[counter]
+        counter = counter + 1
+        suffix = suffix + char
 
-        my_logger.info("MOV - Moving " + source_file + " up a level.")
-        os.rename(source_file, destination_file)
+    number = int(numbers)
 
+    return f"{letters}{delimiter}{number:03}{suffix}{file_extension}"
+
+def search_for_title(input_string, delimiter = "-"):
+    filename, file_extension = os.path.splitext(input_string)
+    filename = filename.upper()
+    file_extension = file_extension.lower()
+    pattern8 = r'^[A-Za-z]{5}\d{3}$'
+    pattern7 = r'^[A-Za-z]{4}\d{3}$'
+    pattern6 = r'^[A-Za-z]{3}\d{3}$'
+    pattern5 = r'^[A-Za-z]{2}\d{3}$'
+
+    results = []
+    filename = re.sub(delimiter, '', filename, flags=re.IGNORECASE)
+    filename_length = len(filename)
+
+    # Search for 8 character codes.
+    counter = 0
+    while counter + 7 < filename_length:
+        input_string = filename[counter:counter + 8]
+        if (re.match(pattern8, input_string)):
+            if my_javlibrary_new_search(input_string):
+                results.append(filename[counter:counter + 8])
+        counter = counter + 1    
+    
+    # Search for 7 character codes.
+    counter = 0
+    while counter + 6 < filename_length:
+        input_string = filename[counter:counter + 7]
+        if (re.match(pattern7, input_string)):
+            if my_javlibrary_new_search(input_string):
+                results.append(filename[counter:counter + 7])
+        counter = counter + 1
+
+    # Search for 6 character codes.
+    counter = 0
+    while counter + 5 < filename_length:
+        input_string = filename[counter:counter + 6]
+        if (re.match(pattern6, input_string)):
+            if my_javlibrary_new_search(input_string):
+                results.append(filename[counter:counter + 6])
+        counter = counter + 1
+
+    # Search for 5 character codes.
+    counter = 0
+    while counter + 4 < filename_length:
+        input_string = filename[counter:counter + 5]
+        if (re.match(pattern5, input_string)):
+            if my_javlibrary_new_search(input_string):
+                results.append(filename[counter:counter + 5])
+        counter = counter + 1
+
+    results = remove_substrings(results)
+
+    return results
+#endregion
+
+#region General Purpose Functions
 def get_console_handler():
    console_handler = logging.StreamHandler(sys.stdout)
    console_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s",datefmt="%Y-%m-%d %H:%M:%S"))
    #console_handler.setFormatter(logging.Formatter("%(message)s"))
    return console_handler
-
-# def get_syslog_handler():
-#    syslog_handler = logging.handlers.SysLogHandler(address=(LOGHOST, 514))
-#    syslog_handler.setFormatter(logging.Formatter("RJMediaScraper %(message)s"))
-#    return syslog_handler
 
 def get_logger():
    logger = logging.getLogger()
@@ -353,6 +338,24 @@ def get_logger():
    logger.addHandler(get_console_handler())
    #logger.addHandler(get_syslog_handler())
    return logger
+
+def remove_substrings(strings):
+    # Sort the strings by length in descending order
+    strings.sort(key=len, reverse=True)
+    
+    # Initialize a list to store non-substring strings
+    result = []
+    
+    # Iterate through the sorted strings
+    for s in strings:
+        # Check if the current string is a substring of any previously added string
+        is_substring = any(s in r for r in result)
+        
+        # If it's not a substring of any previous string, add it to the result
+        if not is_substring:
+            result.append(s)
+    
+    return result
 
 def get_list_of_files():
     folder_list_1 = os.listdir(BASE_DIRECTORY)
@@ -365,6 +368,7 @@ def get_list_of_files():
         folder_list_3.append(file)
 
     return folder_list_3
+#endregion
 
 if __name__ == "__main__":
 
