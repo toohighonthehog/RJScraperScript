f5c2c4e9b168927fea9e58efd780f8ae9b731665
Richard Johnson <richardjj627@gmail.com>
2024-04-21 15:52:23 +0100

    check cookie

--- check_cfcookie.py
+++ check_cfcookie.py
@@ -1,25 +1,22 @@
 # from
 # https://www.whatismybrowser.com/detect/what-is-my-user-agent/
+# this cookie seems to last for only 30 mins
 
 import javscraper, ast
+from datetime import datetime
+
 title = "MIAD-283"
 
-#with open('cookie.json') as f: 
-#    data = f.read()
-#cookie = json.loads(data)
-
-#cookie = {"cf_clearance": "wyE1.YAp9oCgn0U732aTjJOiQ_InFZ3ZT6PcIFrR_ic-1713696506-1.0.1.1-ivUGIH.zI_Fzt5hOjlTrWe4GyAmvecHkkmwsbM.._blOrQCBC2ysg1UXxtcoCREo_dli7TcxWpPJWQp3nPt32w"}
-#print (cookie)
+# load the cookie
 with open("cookie.json", "r") as data:
     cookie = ast.literal_eval(data.read())
-print (cookie)
-#cookie = json.loads(open("c.txt"))
 
-#exit()
-
-#print(type(cookie))
+#print (cookie)
 
 f_my_javlibrary = javscraper.JAVLibrary()
 f_my_javlibrary.debug = True
 f_my_javlibrary._set_cookies(cookie)
+
+# Show the time and run the check
+print (datetime.now())
 print (f_my_javlibrary.search(title))
--- readme.md
+++ readme.md
@@ -113,3 +113,4 @@
 #  Create a mover function/option so that files are moved to a folder based on prate.
 #     We need to be careful with this to ensure the database is kept in sync with the target folder and that the meta data remains consistent.
 #     The idea here is that syncthing only replicates 8+
+#  Think about what happens if cf cookies expire mid process.
